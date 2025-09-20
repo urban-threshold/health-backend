0f1d8900ecacd4940b24c36f571ddfd93ef1d9ac
Patrick McNamara <patrick@Patricks-MacBook-Pro.local>
2025-09-20 14:51:16 +0800

    working on sim now

--- test.py
+++ test.py
@@ -3,4 +3,9 @@ from utils.simulation_manager import HospitalSimulator
 hospital_simulator = HospitalSimulator(1, 10)
 
 # print(hospital_simulator.ed.patients)
-print(hospital_simulator.ICU_ward.patients)
+# print(hospital_simulator.ICU_ward.patients)
+# print(hospital_simulator.simulation_chunks)
+
+for chunk in hospital_simulator.simulation_chunks:
+    print(hospital_simulator.ed.patients)
+    print(f"-----------------------")
--- utils/simulation_manager.py
+++ utils/simulation_manager.py
@@ -2,6 +2,12 @@ from utils.ward import Ward
 from utils.patient import PatientGenerator
 import datetime
 
+class HospitalState:
+    def __init__(self, current_time, wards, ed):
+        self.current_time = current_time
+        self.wards = wards
+        self.ed = ed
+
 class HospitalSimulator:
     def __init__(self, total_sim_hours, sim_time_step_minutes):
         self.start_time = datetime.datetime.now()
@@ -10,13 +16,50 @@ class HospitalSimulator:
         self.simulation_chunks = []
         self.patient_generator = PatientGenerator()
         self.ed = Ward(name="ED", capacity=10, occupied_beds=5, patient_generator=self.patient_generator, is_ed=True)
+        
         self.ICU_ward = Ward(name="ICU", capacity=10, occupied_beds=8, patient_generator=self.patient_generator)
         self.AMU_ward = Ward(name="AMU", capacity=10, occupied_beds=8, patient_generator=self.patient_generator)
         self.CCU_ward = Ward(name="CCU", capacity=10, occupied_beds=8, patient_generator=self.patient_generator)
         self.SW_ward = Ward(name="SW", capacity=10, occupied_beds=8, patient_generator=self.patient_generator)
         self.SSU_ward = Ward(name="SSU", capacity=10, occupied_beds=8, patient_generator=self.patient_generator)
-
         
+        self.wards = [
+            self.ICU_ward,
+            self.AMU_ward,
+            self.CCU_ward,
+            self.SW_ward,
+            self.SSU_ward
+        ]
+
+        self.simulation_chunks.append(
+            HospitalState(
+                current_time=self.start_time,
+                wards=self.wards,
+                ed=self.ed
+            )
+        )
+
+        self.run_simulation()
+
+    def run_simulation(self):
+        current_time = self.start_time
+        while current_time < self.end_time:
+            current_time += self.time_step
+            self.run_simulation_step(current_time)
+
+            self.simulation_chunks.append(
+                HospitalState(
+                    current_time=current_time,
+                    wards=self.wards,
+                    ed=self.ed
+                )
+            )
+
+    def run_simulation_step(self, current_time):
+        self.ed.process_patients(current_time)
+        for ward in self.wards:
+            ward.process_patients(current_time)
+
 
 if __name__ == "__main__":
     hospital_simulator = HospitalSimulator(1, 10)
