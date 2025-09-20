6ff93919b9aa38f26259009651c31345394fa4c9
kevdagoat <kevdagoat@gmail.com>
2025-09-20 10:51:20 +0800

    be(fapi): add poc pseudocode

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
+from asyncio import Queue
+import datetime
+from pydantic import BaseModel
 import uvicorn
 from fastapi import FastAPI
 
 
+class PatientAdmission(BaseModel):
+    name: str
+
+
+class PateintTiemResponse(BaseModel):
+    predictedEntryTs: datetime.datetime
+
+
 def app_factory():
     app = FastAPI(description="Health app API")
+    app.state.q = Queue()
+
+    @app.post("/patient")
+    async def receive_admission(p: PatientAdmission) -> PateintTiemResponse:
+        # ml_model_result = run_ml(p)
+        # update_sim(p, ml_model_result, app.state.q)
+        return #PateintTiemResponse(predictedEntryTs=ml_model_result.ts)
+
     return app
 
 
