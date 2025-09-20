147957289f909bbae906cd1bb5803f669d9a4bc7
kevdagoat <kevdagoat@gmail.com>
2025-09-20 10:30:31 +0800

    be(fapi): stub

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from health-backend!")
+import uvicorn
+from fastapi import FastAPI
+
+
+def app_factory():
+    app = FastAPI(description="Health app API")
+    return app
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app_factory(), host="127.0.0.1", port=8000)
